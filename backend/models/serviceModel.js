65ac4cfcab030ecd813d59ce5945956fd438eb31
antoclus <antolustraducciones@gmail.com>
2023-11-14 18:10:04 +0100

    services working on the backend

--- backend/models/serviceModel.js
+++ backend/models/serviceModel.js
@@ -5,16 +5,10 @@ const mongoose = require("mongoose");
 
 const serviceSchema = new mongoose.Schema(
   {
-    title:{ String,
+    title: String,
     description: String,
     price: String,
   },
-  creator: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', 
-    required: true
-  },
-},
   {
     timestamps: true,
   }
--- client/src/pages/services.js
+++ client/src/pages/services.js
@@ -2,17 +2,39 @@ import React from 'react';
 import "./services.css"
 
 const ServicesPage = () => {
+
+  
   return (
-    <div className="services-page-container">
+  
+    <div className="services-page-container"> 
+    < div className="card"> 
+    <h2> SERVICES</h2>
+    <p> Sign up and log in to your account to have access to your class material and bookings.</p>
+    <p> Contact the teacher to agree on a schedule.</p>
+
+    </div>
       <div className="card">
-        <h2>Book a lesson today!!</h2>
+        <h2>Book an English lesson today!!</h2>
         <a href='./' className='sus'>Get a 1-1 lesson!</a>
+        <p>16 usd/h</p>
+        <button onclick="bookClass()">Book a Class</button>
+        <h2>Book a Spanish lesson today!!</h2>
+        <a href='./' className='sus'>Get a 1-1 lesson!</a>
+        <p>25 usd/h</p>
         <button onclick="bookClass()">Book a Class</button>
       </div>
       <div className="card">
-        <h2>Book a course today!!</h2>
+        <h2>Book an English conversation course today!!</h2>
         <p className='p2'>Get a course today</p>
-        <button onclick="bookCourse()">Book a Course</button>
+        <p className='p2'>Adults</p>
+        <p>30 usd / 4 lessons</p>
+        <button onclick="bookCourse()">Book a Course </button>
+
+        <h2>Book a Spanish conversation course today!!</h2>
+        <p className='p2'>Get a course today</p>
+        <p className='p2'>Adults</p>
+        <p>40 usd / 4 lessons</p>
+        <button onclick="bookCourse()">Book a Course </button>
       </div>
       <div className="card">
         <h2>Get a free Meeting</h2>
